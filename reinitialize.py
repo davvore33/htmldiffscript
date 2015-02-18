47609d7e9909345456309f355b350894e6371356
matteo <davvore33@gmail.com>
2015-02-18 11:45:11 +0100

    new file:   reinitialize.py

---
+++ reinitialize.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python2
+
+import os
+
+localfolder=os.getcwd()
+
+print "this script will reinitialize entire routine"
+
+try:
+	readinglist = open(localfolder+'/list.txt')
+	list = readinglist.read()
+	listS = str.splitlines(list)
+	readinglist.close()
+	os.remove(localfolder+'/list.txt')
+	i=len(listS)
+	while i > 0:
+		try:
+			os.remove(listS[i-1])
+		except:
+			print "there is no "+list[i-1]
+		i=i-1
+except:
+	print "there is no list"
+
+try:
+	os.remove(localfolder+'/site.txt')
+except:
+	print "there is no site"
+try:
+	os.remove(localfolder+'/win.txt')
+except:
+	print "there is no win"
